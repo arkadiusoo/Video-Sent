c8a0272929329581d39ae5e566c784d52fbed949
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-10-22 00:43:30 +0200

    backend cors update

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from downloader.service import download_audio
 from stt.service import transcribe_audio
 from nlp.service import analyze_sentiment
 
 app = Flask(__name__)
+CORS(
+    app,
+    resources={r"/*": {"origins": [
+        "http://localhost:51731",
+        "http://127.0.0.1:51731"
+    ]}},
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "OPTIONS"],
+    supports_credentials=False
+)
 
 @app.route("/health")
 def health():
