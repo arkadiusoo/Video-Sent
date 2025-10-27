ab71df77f41b85ef928c2a67def87ee1b85eac47
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-10-27 18:35:04 +0100

    structure init

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
-export async function analyzeVideo(link) {
-  const res = await axios.post(`${API_URL}/analyze`, { link });
-  return res.data;
-}
